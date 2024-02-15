b4634c3f1492f844f5a90faa268716d575b668ea
micol <micol.benatti@gmail.com>
2024-02-15 13:37:15 +0100

    loading

--- script.js
+++ script.js
@@ -9,3 +9,23 @@ const toggleMenu = () => {
 document.addEventListener('DOMContentLoaded', () => {
     toggle.addEventListener('click', toggleMenu)
 })
+
+//window load
+window.onload = () => {
+    const loading = document.getElementById("loading");
+    const content = document.getElementById("content");
+
+    const hideLoading = () =>{
+        loading.style.display = "none";
+        content.style.opacity = 1;
+        console.log("loaded");
+    };
+    setTimeout(hideLoading, 2000);
+
+};
+
+    
+
+
+
+//aggiungi testo che appare con bottone, accordion
