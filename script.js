e464ea965d604a88b0761e844351a9a7b25262cc
micol <micol.benatti@gmail.com>
2024-02-19 18:44:45 +0100

    text and scrolling

--- script.js
+++ script.js
@@ -21,11 +21,33 @@ window.onload = () => {
         console.log("loaded");
     };
     setTimeout(hideLoading, 2000);
-
 };
 
-    
+/*/read more ma con doppio click, per verificare display
+document.getElementById('read-more-btn').addEventListener('click', () =>{
+    const addText = document.getElementById('add-text');
+    const readMoreBtn = document.getElementById('read-more-btn');
 
+    if(addText.style.display === 'none') {
+        addText.style.display = 'block';
+        readMoreBtn.textContent = 'Nascondi';
+    }
+    else{
+        addText.style.display = 'none';
+        readMoreBtn.textContent = 'Read more';
+    }
+});
+*/
 
+//read more con aggiunta di classe in css per evitare doppio click
+const readMoreBtn = document.getElementById('read-more-btn');
+const addText = document.getElementsByClassName('add-text')[0];
 
-//aggiungi testo che appare con bottone, accordion
+readMoreBtn.addEventListener('click', () => {
+    addText.classList.toggle('show-add-text'); // Aggiungi o rimuovi la classe per mostrare o nascondere il testo aggiuntivo
+    if (readMoreBtn.textContent === 'Read more') {
+        readMoreBtn.textContent = 'Read less';
+    } else {
+        readMoreBtn.textContent = 'Read more';
+    }
+});
